6bebaa8b442050008025fee303b3a5359af95b73
rajkumarmahanty <rajkmahanty@gmail.com>
2019-04-09 14:04:08 +0530

    First Commit For SampleApp

---
+++ DevOpsCCOE/src/com/medtronic/ccoe/devops/HelloDevOps.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.medtronic.ccoe.devops;
+
+/**
+ * @author Raj Kumar Mahanty
+ *
+ */
+public class HelloDevOps {
+	public static void main(String [] args) {
+		System.out.println("Hello DevOps World ...");
+	}
+
+}
---
+++ DevOpsCCOE/src/com/medtronic/ccoe/devops/package-info.java
@@ -0,0 +1,1 @@
+package com.medtronic.ccoe.devops;
